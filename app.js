8d9c60c07d6b3e24898c2b1af94cab81e7389f33
NicolasGautier <nicolas.gautier35135@gmail.com>
2023-02-19 21:22:16 +0100

    update app.js

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
-const fs = require('fs');
+const validator = require('validator');
+const getNotes = require('./notes.js');
 
-fs.writeFileSync('notes.txt', 'This file was created by the app.js file');
+const msg = getNotes();
+console.log(msg);
+
+console.log(validator.isURL('htps://ww.google.com'));
+
+
+
+////////////////////////////////
+// const add = require('./utils.js')
+
+// const sum = add(4, -2);
+
+// console.log(sum);
+
+////////////////////////////////
+//const fs = require('fs');
+
+//use writeFile method to write data to a file
+
+//fs.writeFileSync('notes.txt', 'My name is Andrew.');
+
+//use appendFileSync to append data to a file
+
+//fs.appendFileSync('notes.txt', ' I\'m 44 years old and I live in Vertou.');
+
+//use readFileSync to read data from a file
+// var data = fs.readFileSync('notes.txt', 'utf8');
+// console.log(data);
+
